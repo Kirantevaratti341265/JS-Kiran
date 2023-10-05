b4794cf65ed1b2baf06168a8a8fc5d722f85c960
Kiran Patil <92773815+Kirantevaratti341265@users.noreply.github.com>
2023-10-05 06:04:34 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
