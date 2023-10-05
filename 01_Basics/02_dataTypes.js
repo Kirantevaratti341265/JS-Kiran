976268f4e38f73083951d1c00fa3609434a395ea
Kirantevaratti341265 <92773815+Kirantevaratti341265@users.noreply.github.com>
2023-10-05 03:54:50 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3) 
+
+let name = "kiran"
+let age = 19
+let state = null
+
+// number => 2 to power 53
+// bigint
+// unidentified
+// symbol
+// null
+
+// object
+
+console.log(typeof "kiran");
