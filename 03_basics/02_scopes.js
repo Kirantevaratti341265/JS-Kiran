99cc59078fb0ee9424f0ddd5d5fbdebfbf022b4e
Kiran Patil <92773815+Kirantevaratti341265@users.noreply.github.com>
2023-10-08 06:26:37 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,58 @@
+
+let a = 300
+
+if (true) {
+    let a = 10
+    const b = 20
+  
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const userName = "Kiran"
+
+    function two(){
+        const website = "youtubr"
+        console.log(userName)
+    }
+
+    // console.log(website);
+    
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "kiran"
+    if (username === "kiran"){
+    const website = "youtube"
+// console.log(username + website);
+}
+
+// console.log(website);
+}
+
+// console.log(username)
+
+
+// +++++++++ interesting ++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+ }
+
+ 
+
+ addTwo(5)
+
+ const addTwo = function(num){
+    return num + 2
+ }
+
+
